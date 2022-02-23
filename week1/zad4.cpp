e2cf1a548dec515c22dd5ef5b512f6fe3f3da246
iDimitorva1 <dimitrova.a.irina@gmail.com>
2022-02-23 20:49:15 +0200

    Update

--- week1/zad4.cpp
+++ week1/zad4.cpp
@@ -5,10 +5,37 @@
 
 using namespace std;
 
+static int i{0};
+
+void IsPalindrome (char* str, int counter)
+{
+    if(i == counter/2 && str[i] == str[counter-1-i])
+    {
+        i++;
+        IsPalindrome (str, counter);
+    }
+    else if(i < counter/2 && str[i] == str[counter-1-i])
+    {
+        cout << "Palindrome.";
+    }
+    else
+    {
+        cout << "Not a palindrome.";
+    }
+}
+
 int main()
 {  
     char str [100];
     cin.getline (str, 100);
+
+    int counter{0};
+    while(str[counter] != '\0')
+    {
+        counter++;
+    }
+
+    IsPalindrome(str, counter);
 
     return 0;
 }
